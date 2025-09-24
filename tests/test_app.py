defaf4c485818145ae638aaea0fe118c5ac5a07f
Caroline Derolf <cnderolf325@gmail.com>
2025-09-24 13:47:09 -0400

    Fix Files + Test Test

--- tests/test_app.py
+++ tests/test_app.py
@@ -22,22 +22,15 @@ def test_multiply(client):
     response = client.get("/multiply?a=5&b=7")
     assert b"35" in response.data
 
-def test_multiply_fail(client):
-    response = client.get("/multiply?a=6&b=7")
-    assert b"35" in response.data
+# def test_multiply_fail(client):
+#     response = client.get("/multiply?a=6&b=7")
+#     assert b"35" in response.data
 
-def test_subtract_fail(client):
-    response = client.get("/subtract?a=7&b=7")
-    assert b"1" in response.data
+# def test_subtract_fail(client):
+#     response = client.get("/subtract?a=7&b=7")
+#     assert b"1" in response.data
 
-def test_add_fail(client):
-    response = client.get("/add?a=5&b=6")
-    assert b"10" in response.data
-
-# addition = [(a,b,a+b) for a in range(-10,10) for b in range(-100, 100)]
-
-# @pytest.mark.parametrize("a,b,expected", addition)
-# def test_add_parameterized(client, a, b, expected):
-#     response = client.get(f"/add?a={a}&b={b}")
-#     assert str(expected).encode() in response.data
+# def test_add_fail(client):
+#     response = client.get("/add?a=5&b=6")
+#     assert b"10" in response.data
 
